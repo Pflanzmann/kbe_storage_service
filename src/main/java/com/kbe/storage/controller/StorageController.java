24169d79a3eaa7e73fd6da90c30421c02612c33e
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-06 16:43:04 +0100

    Add postNewInfos call

--- src/main/java/com/kbe/storage/controller/StorageController.java
+++ src/main/java/com/kbe/storage/controller/StorageController.java
@@ -3,16 +3,14 @@ package com.kbe.storage.controller;
 import com.kbe.storage.model.GifInformation;
 import com.kbe.storage.repository.GifInformationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/storage")
+@RequestMapping("/information")
 public class StorageController {
 
     GifInformationRepository gifInformationRepository;
@@ -25,5 +23,11 @@ public class StorageController {
     @GetMapping
     public List<GifInformation> getAllInfos() {
         return gifInformationRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<GifInformation> postNewInfos(@RequestBody GifInformation gifInformation) {
+        return ResponseEntity.ok(gifInformationRepository.save(gifInformation));
     }
 }
