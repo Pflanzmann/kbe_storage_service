ecae129cbcc90a773550e46b58799692031a4e1d
Ronny Brzeski <Svenbrzeski3@freenet.de>
2022-02-06 16:04:42 +0100

    Init repo

---
+++ src/main/java/com/kbe/storage/model/GifInformation.java
@@ -0,0 +1,67 @@
+package com.kbe.storage.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class GifInformation {
+
+    @Id
+    private UUID id;
+    private String title;
+    private String author;
+    private String description;
+    private String topic;
+
+    public GifInformation(UUID id, String title, String author, String description, String topic) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.topic = topic;
+    }
+
+    public GifInformation() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
